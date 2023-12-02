e2ea5644a706ccdd8136a4a9c35f9179b0d1a5c9
Marco Zuniga U <mazuga92@gmail.com>
2023-12-02 12:33:47 -0600

    Pagination

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ async function bootstrap() {
       {
         whitelist: true,
         forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions:{
+          enableImplicitConversion: true
+        }
       }
     )
   )
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance =axios;
-
   constructor(
   @InjectModel(Pokemon.name)
-  private readonly pokemonModel: Model<Pokemon>
+  private readonly pokemonModel: Model<Pokemon>,
+  private readonly http: AxiosAdapter,
 
   ){}
 
@@ -22,8 +22,8 @@ export class SeedService {
     await this.pokemonModel.deleteMany({}); //delete all
 
 
-    const {data }= await 
-    this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await 
+    this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert:{name:string, no:number} [] = [];
 
@@ -42,3 +42,21 @@ export class SeedService {
   }
   
 }
+
+// async executeSEEDs() {
+//   await this.pokemonModel.deleteMany({}); //delete all
+//   const { data } = await firstValueFrom(
+//     this.httpService
+//       .get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=50')
+//       .pipe(
+//         catchError((error: AxiosError) => {
+//           console.error(error.response.data);
+//           throw 'An error happened!';
+//         }),
+//       ),
+//   );
+//   return data;
+// }
+
+
+
